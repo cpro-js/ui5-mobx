217a0deaaeee381cee1e9c7b1c6efc70539fe333
Torsten <torsten.manhardt@gmx.de>
2023-04-17 10:50:58 +0200

    fix: avoid exception when path isn't solved

--- src/cpro/js/ui5/mobx/MobxModel.ts
+++ src/cpro/js/ui5/mobx/MobxModel.ts
@@ -74,7 +74,12 @@ export class MobxModel<T extends object> extends Model implements MobxModelType<
     // retrieve the value by recursively traversing the path parts
     const parts = resolvedPath.substring(1).split("/");
     return parts.reduce((node: any, current: string) => {
-      return node[current];
+      if (node && current) {
+        return node[current];
+      }
+      else {
+        return null;
+      }
     }, this.observable);
   }
 
